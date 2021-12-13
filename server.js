dcb0ba5f45623adf0938ac123c4490819408fc47
Jason <LutzJason92@gmail.com>
2021-12-12 22:36:02 -0700

    Update server.js
    began writing prompts and functions for each user selection

--- server.js
+++ server.js
@@ -4,9 +4,10 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const figlet = require("figlet");
+const Connection = require("mysql2/typings/mysql/lib/Connection");
 requirer("console.table");
 
-const mysql = mysql.createConnection({
+const dbconnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
@@ -14,13 +15,13 @@ const mysql = mysql.createConnection({
   port: 3306,
 });
 
-mysql.connect((err) => {
+dbconnection.connect((err) => {
   if (err) throw err;
   console.log("!!!now connected to EMP_CMS2_db!!!");
   init();
 });
 
-const startQuestions = [
+const initOptions = [
   // when thens
   "View all Departments",
   "View all Roles",
@@ -29,10 +30,81 @@ const startQuestions = [
   "Add a Role",
   "Add an Employee",
   "Update an Employee Role",
+  "Delete an Employee",
+  "Exit CMS",
 ];
 
 // inquirer prompts go here
 const init = () => {
   console.clear();
   logo("JDL CMS APP");
+
+  inquirer
+    .prompt({
+      name: "userOption",
+      type: "list",
+      message: "Welcome to JDL CMS APP. Select From the following options:",
+      choices: initOptions,
+    })
+    .then((optionSelections) => {
+      switch (optionSelections.userOption) {
+        // "View all Departments",
+        case "View all Departments":
+          viewAllDeps();
+          break;
+        // "View all Roles",
+        case "View all Roles":
+          viewAllRoles();
+          break;
+        // "View all Employees",
+        case "View all Employees":
+          viewAllEmps();
+          break;
+        // "Add a Department",
+        case "Add a Department":
+          addDep();
+          break;
+        // "Add a Role",
+        case "View all Departments":
+          addRole();
+          break;
+        // "Add an Employee",
+        case "View all Departments":
+          addEmp();
+          break;
+        // "Update an Employee Role",
+        case "Update an Employee Role":
+          updateEmp();
+          break;
+        // "Delete an Employee",
+        case "Delete an Employee":
+          destroyEmp();
+          break;
+        // "Exit CMS",
+        case "Exit CMS":
+          console.clear();
+          logo("Thank You for using JDL CMS APP");
+          connection.end();
+          break;
+      }
+    });
 };
+
+const viewAllDeps = () => {
+  logo("View All Departments:");
+  mysql.uery();
+};
+
+viewAllRoles();
+
+viewAllEmps();
+
+addDep();
+
+addRole();
+
+addEmp();
+
+updateEmp();
+
+destroyEmp();
