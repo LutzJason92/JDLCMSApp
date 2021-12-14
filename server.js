006856d6ea4144fb8aa55564729fb59fa1e1d1ad
Jason <LutzJason92@gmail.com>
2021-12-13 21:11:21 -0700

    Add emp, add role and add dep created
    wrote functions to create department, employee and role

--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const figlet = require("figlet");
-const Connection = require("mysql2/typings/mysql/lib/Connection");
-requirer("console.table");
+// const Connection = require("mysql2/typings/mysql/lib/Connection");
+require("console.table");
 
-const dbconnection = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
@@ -15,7 +15,7 @@ const dbconnection = mysql.createConnection({
   port: 3306,
 });
 
-dbconnection.connect((err) => {
+db.connect((err) => {
   if (err) throw err;
   console.log("!!!now connected to EMP_CMS2_db!!!");
   init();
@@ -36,16 +36,17 @@ const initOptions = [
 
 // inquirer prompts go here
 const init = () => {
-  console.clear();
-  logo("JDL CMS APP");
+  // console.clear();
+  console.log("JDL CMS APP");
 
   inquirer
     .prompt({
       name: "userOption",
       type: "list",
-      message: "Welcome to JDL CMS APP. Select From the following options:",
+      message: "Select From the following options:",
       choices: initOptions,
     })
+
     .then((optionSelections) => {
       switch (optionSelections.userOption) {
         // "View all Departments",
@@ -65,11 +66,11 @@ const init = () => {
           addDep();
           break;
         // "Add a Role",
-        case "View all Departments":
+        case "Add a Role":
           addRole();
           break;
         // "Add an Employee",
-        case "View all Departments":
+        case "Add an Employee":
           addEmp();
           break;
         // "Update an Employee Role",
@@ -83,7 +84,7 @@ const init = () => {
         // "Exit CMS",
         case "Exit CMS":
           console.clear();
-          logo("Thank You for using JDL CMS APP");
+          console.log("Thank You for using JDL CMS APP");
           connection.end();
           break;
       }
@@ -91,20 +92,155 @@ const init = () => {
 };
 
 const viewAllDeps = () => {
-  logo("View All Departments:");
-  mysql.uery();
+  console.log("View All Departments:");
+  db.query(
+    `SELECT * FROM Departments
+    `,
+    (err, res) => {
+      if (err) throw err;
+      console.log("\n");
+      console.table(
+        "Displaying Departments and thier respective Identification Numbers",
+        res
+      );
+    }
+  );
+  init();
 };
 
-viewAllRoles();
+const viewAllRoles = () => {
+  console.log("View All Roles:");
+  db.query(
+    `SELECT * FROM Roles
+    `,
+    (err, res) => {
+      if (err) throw err;
+      console.log("\n");
+      console.table("Displaying all Roles", res);
+    }
+  );
+  init();
+};
+const viewAllEmps = () => {
+  console.log("View All Roles:");
+  db.query(
+    `SELECT * FROM Employees
+    `,
+    (err, res) => {
+      if (err) throw err;
+      console.log("\n");
+      console.table("Displaying all Employees", res);
+    }
+  );
+  console.log("Select another Operation");
+  init();
+};
 
-viewAllEmps();
+const addDep = () => {
+  // console.clear();
+  console.log("Add a Department:");
+  inquirer
+    .prompt({
+      name: "addDepartment",
+      type: "input",
+      message: "What is the name of the Department being added?",
+    })
+    .then((res) => {
+      db.query(
+        `INSERT INTO Departments (dep_name) VALUES (?);`,
+        res.addDepartment,
+        function (err) {
+          if (err) throw err;
+          console.table(res);
+          init();
+        }
+      );
+    });
+};
 
-addDep();
+const addRole = () => {
+  // console.clear();
+  console.log("Add a Role:");
+  inquirer
+    .prompt([
+      {
+        name: "addTitle",
+        type: "input",
+        message: "What is the Title of this Role?",
+      },
+      {
+        name: "addSalary",
+        type: "input",
+        message: "What is the salary fro this position?",
+      },
+      {
+        name: "addDepId",
+        type: "input",
+        message: "What is the predetermined Department ID?",
+      },
+    ])
+    .then((res) => {
+      db.query(
+        `INSERT INTO Roles SET ?`,
+        {
+          title: res.addTitle,
+          salary: res.addSalary,
+          dep_id: res.addDepId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(res);
+          console.log("Role Added Successfully");
+          init();
+        }
+      );
+    });
+};
 
-addRole();
+const addEmp = () => {
+  // console.clear();
+  console.log("to ADD an Employee, answer the following questions:");
+  inquirer
+    .prompt([
+      {
+        name: "addEmpFirst",
+        type: "input",
+        message: "What is their FIRST name?",
+      },
+      {
+        name: "addEmpLast",
+        type: "input",
+        message: "What is their LAST name?",
+      },
+      {
+        name: "empRoleId",
+        type: "input",
+        message: "Enter thier predefined Role ID?",
+      },
+      {
+        name: "empManager",
+        type: "input",
+        message: "Enter thier Managers predefined ID?",
+      },
+    ])
+    .then((res) => {
+      db.query(
+        `INSERT INTO Employees SET ?`,
+        {
+          first_name: res.addEmpFirst,
+          last_name: res.addEmpLast,
+          role_id: res.empRoleId,
+          manager_id: res.empManager,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(res);
+          init();
+        }
+      );
+    });
+};
 
-addEmp();
+// updateEmp();
 
-updateEmp();
-
-destroyEmp();
+// destroyEmp();
